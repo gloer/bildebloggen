6f33e86e1fc0eea6f6de2645bd9c528e13063913
Gløer Olav Langslet <gloer@langslet.com>
2019-01-22 11:10:33 +0100

    Bildeblogg med Firebase

---
+++ index.js
@@ -0,0 +1,63 @@
+// HTML-elementer
+const inpBilde = document.querySelector("#inpBilde");
+const inpTekst = document.querySelector("#inpTekst");
+const skjema = document.querySelector("#skjema");
+const overlay = document.querySelector("#overlay");
+
+const zoom_overlay = document.querySelector("#zoom_overlay");
+const zoom_image = zoom_overlay.querySelector("img");
+const zoom_close = zoom_overlay.querySelector("div");
+
+
+const main = document.querySelector("main");
+
+// Firebase
+const db = firebase.database();
+const storage = firebase.storage();
+
+const bloggen = db.ref("bloggen");
+
+
+// Funksjon som lagrer bilde i databasen
+function lagreBilde(evt) {
+    evt.preventDefault();
+
+    // Viser overlay
+    overlay.style.display = "flex";
+
+    // Bilde som skal lastes opp
+    const bilde = inpBilde.files[0];
+
+    // Hvor skal vi lagre bildet
+    const lagringsplass = storage.ref("bloggbilder/" + ( +new Date() ) + bilde.name);
+
+    // Vi laster opp bildet til storage
+    lagringsplass.put(bilde)
+        .then( bilde => bilde.ref.getDownloadURL() )
+        .then( url => {
+            bloggen.push({
+                url: url,
+                tekst: inpTekst.value
+            });
+            skjema.reset();
+            overlay.style.display = "none";
+        } );
+
+}
+
+function visBilde(snap) {
+    const key = snap.key;
+    const data = snap.val();
+
+    main.innerHTML = `
+        <article>
+            <img src="${data.url}">
+            <p>${data.tekst}</p>
+        </article>
+    ` + main.innerHTML;
+}
+
+
+// Event Listeners
+skjema.addEventListener("submit", lagreBilde);
+bloggen.on("child_added", visBilde);
